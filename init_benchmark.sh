0ca1923cbc2e452118d9676cd44b53f768e3042c
Steevan BARBOYON <steevan.barboyon@gmail.com>
2018-12-18 12:35:25 +0000

    Clear cache manually

--- init_benchmark.sh
+++ init_benchmark.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 
-function init() {
-    composer install --no-dev -n
+function clearCache() {
+    sudo /bin/rm -rf storage/cache/*
     [ "$?" != "0" ] && exit 1
+
+    sudo /bin/chmod -R 777 storage/cache
+    [ "$?" != "0" ] && exit 1
+}
+
+function init() {
+    clearCache
+    composer install --no-dev --classmap-authoritative
 
     return 0
 }
