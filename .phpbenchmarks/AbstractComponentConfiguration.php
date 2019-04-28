30b0fc6de63f527e4b2dc6b66e24fb5bf6345af0
Steevan <steevan.barboyon@gmail.com>
2019-04-28 17:31:46 +0200

    Hello world benchmark for Symlex 4.2

---
+++ .phpbenchmarks/AbstractComponentConfiguration.php
@@ -0,0 +1,86 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AbstractComponentConfiguration;
+
+abstract class AbstractComponentConfiguration
+{
+    public static function getComponentType(): int
+    {
+        return 2;
+    }
+
+    public static function getComponentName(): string
+    {
+        return 'Symlex';
+    }
+
+    public static function getComponentSlug(): string
+    {
+        return 'symlex';
+    }
+
+    public static function isPhp56Enabled(): bool
+    {
+        return false;
+    }
+
+    public static function isPhp70Enabled(): bool
+    {
+        return false;
+    }
+
+    public static function isPhp71Enabled(): bool
+    {
+        return false;
+    }
+
+    public static function isPhp72Enabled(): bool
+    {
+        return true;
+    }
+
+    public static function isPhp73Enabled(): bool
+    {
+        return true;
+    }
+
+    public static function getBenchmarkUrl(): string
+    {
+        return 'benchmark/helloworld';
+    }
+
+    public static function getCoreDependencyName(): string
+    {
+        return 'symlex/symlex-core';
+    }
+
+    public static function getCoreDependencyMajorVersion(): int
+    {
+        return 4;
+    }
+
+    public static function getCoreDependencyMinorVersion(): int
+    {
+        return 2;
+    }
+
+    public static function getCoreDependencyPatchVersion(): int
+    {
+        return 0;
+    }
+
+    public static function getBenchmarkType(): int
+    {
+        return 1;
+    }
+
+    public static function getSourceCodeUrls(): array
+    {
+        return [
+            'route' => 'https://github.com/phpbenchmarks/symlex-common/blob/4.1.0/src/Controller/BenchmarkController.php#L12',
+            'controller' => 'https://github.com/phpbenchmarks/symlex-common/blob/4.1.0/src/Controller/BenchmarkController.php'
+        ];
+    }
+}
